fe7c9e415271bfb7e76454292d6379e20fd9cecc
Moussaoui <mohamed.moussaoui@ext.soprasteria.com>
2022-04-04 23:28:34 +0200

    new script

--- r_place.user.js
+++ r_place.user.js
@@ -12,7 +12,7 @@
 
 const images = [
   /* [x_location, y_location, scale, image_url] */
-  [0, 429, 1, "https://hicham-belhseine.github.io/rplace-map/images/morocco_place.png"],
+  [0, 429, 1, "https://github.com/mmed199/rplace_test/blob/master/images/tajine.png"],
   [1624, 799, 1, "https://raw.githubusercontent.com/Hicham-Belhseine/rplace-flag/main/images/morocco_place.png"]
 ]
 
